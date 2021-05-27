47c1eabcf0c49ca9e9761091f1b96d96fca92359
DEVANSH PATEL <78786952+devansh27201@users.noreply.github.com>
2021-05-27 16:04:34 +0530

    Add files via upload
    This validation.js file is for the html registration form task that was given on first day.[EXTRA]

---
+++ validate.js
@@ -0,0 +1,74 @@
+function registration()
+{
+
+    var name= document.getElementById("t1").value;
+    var email= document.getElementById("t2").value;
+    var uname= document.getElementById("t3").value;
+    var pwd= document.getElementById("t4").value;			
+    var cpwd= document.getElementById("t5").value;
+    
+    
+    
+    var pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    var letters = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+   
+    if(name=='')
+    {
+        alert('Please enter your name');
+    }
+    
+    else if(email=='')
+    {
+        alert('Please enter your user email id');
+    }
+    else if (!filter.test(email))
+    {
+        alert('Invalid email');
+    }
+    else if(uname=='')
+    {
+        alert('Please enter the user name.');
+    }
+
+    else if(pwd=='')
+    {
+        alert('Please enter Password');
+    }
+    else if(cpwd=='')
+    {
+        alert('Enter Confirm Password');
+    }
+    else if(!pwd_expression.test(pwd))
+    {
+        alert ('Upper case, Lower case, Special character and Numeric letter are required in Password filed');
+    }
+    else if(pwd != cpwd)
+    {
+        alert ('Password not Matched');
+    }
+    else if(document.getElementById("t5").value.length < 6)
+    {
+        alert ('Password minimum length is 6');
+    }
+    else if(document.getElementById("t5").value.length > 12)
+    {
+        alert ('Password max length is 12');
+    }
+   
+    else
+    {				                            
+           alert('Thank You for Registering on my site.');
+           window.location = "http://www.google.com";
+          
+    }
+}
+function clearFunc()
+{
+    document.getElementById("t1").value="";
+    document.getElementById("t2").value="";
+    document.getElementById("t3").value="";
+    document.getElementById("t4").value="";
+    document.getElementById("t5").value="";
+    
+}
